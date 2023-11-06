562ff43ded2453dc64fbb9b238b9f49915bfca78
Pedro Henrique Milani Vagula <pedrohenriquehmv@gmail.com>
2023-11-06 12:11:48 -0300

    pages err fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import styles from '@/styles/Home.module.css'
 import Image from 'next/image';
-import Card from './components/Card';
+import Card from '../../components/Card';
 
 export async function getStaticProps() {
     const maxPokemon = 151;
